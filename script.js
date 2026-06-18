306af0fa5896c22f694c79a2e7ccaff29bc6801f
Muhsur <muhamadsururi642@gmail.com>
2026-06-18 16:13:12 +0700

    commit : add file html.php.css.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hai anjing, coment yang bener!");
+let belajar = "oya ya belajar web";
+/*console.log(belajar)*/
+function halo(){
+  let bel = document.getElementById("tombol").value;
+  alert("comentar : " + bel);
+}
+
+
